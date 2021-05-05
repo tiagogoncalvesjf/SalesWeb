3591b63a38a9450e4cf3c41b270dfc2898818901
tiagogoncalvesjf <79933395+tiagogoncalvesjf@users.noreply.github.com>
2021-05-05 14:49:24 -0300

    Migration

--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -37,7 +37,9 @@ namespace SalesWebMVC
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SalesWebMvcContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
+                    builder.MigrationsAssembly("SalesWebMVC")));
+                    /* OBS                          /\ - mesmo nome da solução do Projeto, neste caso SalesWebMVC  */
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
